513ed7b028317d10be454d60e9ac0ce900f8ccaf
feiteiradev <feiteiradev@users.noreply.github.com>
2026-09-18 14:49:07 +0000

    deploy: 5b2b0e3339bcad77a73b2c24e7b8eba8e88940b5

--- _next/static/kZcTyobA1RxD1NlhYCTLz/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002F[page]","\u002F[locale]\u002F[page]\u002F[slug]","\u002F[locale]\u002Fapps\u002F[app]","\u002F[locale]\u002Fapps\u002F[app]\u002F[doc]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
